37b14894c5f530d0379da37473122cbdee9d4d00
Bozhidar Yunchov <bobi.iv.yu@gmail.com>
2021-05-05 17:20:04 +0300

    Fixed bug with file handling

--- api/helpers/analysis.py
+++ api/helpers/analysis.py
@@ -5,6 +5,8 @@ from zipfile import ZipFile
 from os import walk
 import json
 
+ALLOWED_FILE_EXTENTIONS = ('.xls','.xlsx', '.ods', '.odf' ,'.odt', '.csv')
+
 
 class NpEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -20,9 +22,62 @@ class NpEncoder(json.JSONEncoder):
 
 class PlatformDataAnalyser():
 
-    def __init__(self, content_dir):
-        self.content_dir = content_dir
-        self.allowed_file_extentions = ('.xls','.xlsx', '.ods', '.odf' ,'.odt', '.csv')
+ 
+    def __init__(self, upload_path):
+        self.upload_path = upload_path
+
+        self.student_results = None
+        self.system_logs = None
+
+        self.results_df = []
+        self.logs_df = []
+
+        self.init_data_from_file()
+
+
+    def init_data_from_file(self):
+        for content in walk(self.upload_path):
+            
+            #Check if there are any files in the current dir
+            if content[2]:
+                # if there are files, loop through all and check if they are of the allowed types
+                for item in content[2]:
+                    data_file = f'{content[0]}/{item}'
+
+                    if item.endswith('.zip'):
+                        self.handle_zip_file(data_file)
+                    elif item.endswith(ALLOWED_FILE_EXTENTIONS):
+                        self.handle_data_file(data_file)
+
+        if self.results_df and self.logs_df:
+            self.student_results = pd.concat(self.results_df).sort_values(by='ID', ascending=True)
+            self.system_logs = pd.concat(self.logs_df)
+
+            del self.results_df
+            del self.logs_df
+
+
+    def handle_zip_file(self, path_to_file):
+        with ZipFile(path_to_file) as input_archive:
+            for item in input_archive.namelist():
+                if item.endswith(ALLOWED_FILE_EXTENTIONS):
+                    with input_archive.open(item) as data_file:
+                        self.handle_data_file(data_file)
+
+
+    def handle_data_file(self, path_to_file):
+        df = pd.read_csv(path_to_file) if path_to_file.endswith('.csv') else pd.read_excel(path_to_file)
+
+        if 'ID' and 'Result' in df.columns:
+            self.results_df.append(df)
+            print('Result file found! Appending DF to list.')
+        elif 'Event' and 'Component' in df.columns:
+            df['User ID'] = df['Description'].apply(lambda x: np.int32(re.sub(r"[\D]+",' ', x).strip().split()[0]))
+            self.logs_df.append(df)
+            print('Log file found! Appending DF to list.')
+        else:
+            print('Nothing found!')
+
 
     def handle_individual_files(self):
         results_df = []
@@ -60,9 +115,8 @@ class PlatformDataAnalyser():
             return (None, None)
 
     def calculate_central_tendency(self, selector):
-        student_results, system_logs = self.handle_individual_files()
 
-        if not student_results.empty and not system_logs.empty:
+        if not self.student_results.empty and not self.system_logs.empty:
             from statistics import mode
 
             if selector == 'all':
@@ -71,7 +125,7 @@ class PlatformDataAnalyser():
             event_context = f'Assignment: Качване на Упр. {selector}'
             event_name = 'Submission created.'
 
-            filtered_data = system_logs[system_logs['Event context'].str.contains(event_context) & (system_logs['Event name'] == event_name)]['Description']
+            filtered_data = self.system_logs[self.system_logs['Event context'].str.contains(event_context) & (self.system_logs['Event name'] == event_name)]['Description']
             
             uploded_files = np.empty_like(filtered_data, dtype=int)
 
