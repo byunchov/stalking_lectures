dc726f6829a7aea08ae79035a5faaca10727a908
Ivan <ivansandev82@gmail.com>
2021-05-05 15:29:39 +0200

    technical updates to analyzer

--- api/helpers/analysis.py
+++ api/helpers/analysis.py
@@ -5,7 +5,7 @@ from zipfile import ZipFile
 from os import walk
 import json
 
-
+ALLOWED_EXTENTIONS = ('.xls','.xlsx', '.ods', '.odf' ,'.odt', '.csv')
 class NpEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, np.integer):
@@ -22,57 +22,67 @@ class PlatformDataAnalyser():
 
     def __init__(self, content_dir):
         self.content_dir = content_dir
-        self.allowed_file_extentions = ('.xls','.xlsx', '.ods', '.odf' ,'.odt', '.csv')
+        self.err_log = []
+        self.student_results = None
+        self.system_logs = None
+
+        status = False
+        try:
+            if content_dir.endswith(".zip"):
+                pass
+            else:
+                status = self.handle_individual_files()
+        except:
+            self.err_log.append("[ERR]: Fatar error occured while initializing file data.")
+
+        if not status:
+            self.err_log.append("[ERR]: Error occured while initializing data. It's possible that the data of the file doesn't match the requirements.")
 
     def handle_individual_files(self):
+        # returns True if handling is successful, else False`
         results_df = []
         logs_df = []
 
         for content in walk(self.content_dir):
             if content[2]:
                 for item in content[2]:
-                    if item.endswith(self.allowed_file_extentions):
+                    if item.endswith(ALLOWED_EXTENTIONS):
                         data_file = f'{content[0]}/{item}'
-                        if data_file.endswith(self.allowed_file_extentions[-1]):                    
-                            df = pd.read_csv(data_file)
-                        else:
-                            df = pd.read_excel(data_file)
+                        df = pd.read_csv if data_file.name.endswith('.csv') else pd.read_excel(data_file)
 
                         if 'ID' and 'Result' in df.columns:
                             results_df.append(df)
-                            print('Result file found! Appending DF to list.')
                         elif 'Event' and 'Component' in df.columns:
                             df['User ID'] = df['Description'].apply(lambda x: np.int32(re.sub(r"[\D]+",' ', x).strip().split()[0]))
                             logs_df.append(df)
-                            print('Log file found! Appending DF to list.')
                         else:
-                            print('Nothing found!')
-        
+                            # TODO: handle error if columns data doesn't match
+                            return False
+
         if results_df and logs_df:
-            student_results = pd.concat(results_df).sort_values(by='ID', ascending=True)
-            system_logs = pd.concat(logs_df)
+            self.student_results = pd.concat(results_df).sort_values(by='ID', ascending=True)
+            self.system_logs = pd.concat(logs_df)
 
             del results_df
             del logs_df
-            
-            return (student_results, system_logs)
+
+            return True
         else:
-            return (None, None)
+            return False
 
     def calculate_central_tendency(self, selector):
-        student_results, system_logs = self.handle_individual_files()
+        # redundant, already checked in initialization
+        # if not self.student_results.empty and not self.system_logs.empty:
+        from statistics import mode
 
-        if not student_results.empty and not system_logs.empty:
-            from statistics import mode
-
-            if selector == 'all':
-                selector = ''
+        if selector == 'all':
+            selector = ''
 
             event_context = f'Assignment: Качване на Упр. {selector}'
             event_name = 'Submission created.'
 
-            filtered_data = system_logs[system_logs['Event context'].str.contains(event_context) & (system_logs['Event name'] == event_name)]['Description']
-            
+            filtered_data = self.system_logs[self.system_logs['Event context'].str.contains(event_context) & (self.system_logs['Event name'] == event_name)]['Description']
+
             uploded_files = np.empty_like(filtered_data, dtype=int)
 
             for index, item in enumerate(filtered_data):
@@ -93,6 +103,6 @@ class PlatformDataAnalyser():
             uploded_files_stats['median'] = int(np.median(uploded_files))
 
             return json.dumps(uploded_files_stats, cls=NpEncoder)
-        
+
         else:
             return json.dumps({ 'error': 'Nohing to be dispaleyd!' })
